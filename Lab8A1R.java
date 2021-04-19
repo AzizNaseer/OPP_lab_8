d5889f71da661dcc2e6819f86dabfdcb930dca6b
AzizNaseer <80484925+AzizNaseer@users.noreply.github.com>
2021-04-20 04:47:40 +0500

    Update Lab8A1R.java

--- Lab8A1R.java
+++ Lab8A1R.java
@@ -1,11 +1,30 @@
 package Lab8;
 
+import java.util.Scanner;
+
 public class Lab8A1R {
     public static void main(String[] args) {
-        book B= new book("Fifty shades",999,88);
-        tape T= new tape("mp3",899,99);
+        System.out.println("Enter title of book");
+        Scanner input= new Scanner(System.in);
+        Scanner i= new Scanner(System.in);
+        String x=i.nextLine();
+        System.out.println("enter price of book");
+        int y= input.nextInt();
+        System.out.println("enter the page count");
+        int z=input.nextInt();
+        book B= new book();
+
+        B.setTitle(x) ; B.setPrice(y); B.setPageCount(z);
+        System.out.println("Enter title of Tape");
+        x=i.nextLine();
+        System.out.println("enter price of tape");
+        y= input.nextInt();
+        System.out.println("enter the Playing time");
+        z=input.nextInt();
+        tape T= new tape();
+        T.setTitle(x); T.setPrice(y); T.setPlayingTime(z);
         B.display();
-        System.out.println("------------------------------");
         T.Display();
     }
 }
+
